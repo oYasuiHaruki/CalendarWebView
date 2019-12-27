9a26f02077f83054a50304271a090db12b28550b
haruki.yasui <u219573f@icloud.com>
2019-12-27 18:36:42 +0900

    Initial Commit

---
+++ acquireLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  acquireLocation
+//
+//  Created by yasui.haruki on 12/27/19.
+//  Copyright © 2019 haruki.yasui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
